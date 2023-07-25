ea06c19ef1b04381adaf57fbedcd7a0aeca3f452
JDBetteridge <j.betteridge@imperial.ac.uk>
2023-07-25 12:03:18 +0100

    Updates after live test

--- firedrake/standard_firedrake_build.sh
+++ firedrake/standard_firedrake_build.sh
@@ -2,11 +2,9 @@
 
 curl -O https://raw.githubusercontent.com/firedrakeproject/firedrake/master/scripts/firedrake-install
 
-source /data/home/jb1581/workspace/pileus/firedrake/standard_variables
+source /data/shared/pileus/firedrake/standard_variables
 
-echo PETSC_DIR is $PETSC_DIR
-
-python3.11 firedrake-install \
+$PYTHON firedrake-install \
     --no-package-manager \
     --honour-petsc-dir \
     --mpicc=$MPICC \
